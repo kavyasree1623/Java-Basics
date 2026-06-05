d696d8c742777ba4de02bd4a1adf654c1761c079
Kavya Sree <goli.kavyasree1612@gmail.com>
2026-06-05 11:56:06 +0530

    Add Variables class with main method

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Sree";
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
